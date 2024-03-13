55aa4afc4d62546aba19920edd9af9d3dd0952ce
hyoon1998 <hya62@sfu.ca>
2024-03-13 14:22:01 -0700

    change shape sizes

--- new-portfolio/src/components/Hero.tsx
+++ new-portfolio/src/components/Hero.tsx
@@ -11,7 +11,7 @@ function Hero({ x, y }: { x: number, y: number }): JSX.Element {
         moonAnimation()
         starAnimation()
         starAnimation2()
-    }, [])
+    },[] )
 
     async function starAnimation() {
         await animateStar(scopeStar.current, { opacity: 1, filter: 'blur(4px)' }, { duration: 1, delay: 1.5 })
@@ -39,11 +39,11 @@ function Hero({ x, y }: { x: number, y: number }): JSX.Element {
         <>
             <div style={{ top: y, left: x }} className={`h-[100px] w-[100px] -z-10 fixed -translate-x-1/2 -translate-y-1/2 rounded-full blur-3xl bg-cyan-400`}></div>
             <div className='h-screen fixed bg-[linear-gradient(20deg,rgba(0,0,0,1),rgba(19,8,106,1),rgba(0,0,0,1))] w-screen -z-50'>
-                <img className='fixed top-1/4 right-40 blur-sm opacity-0' ref={scopeMoon} src={'icons8-moon-90.png'} />
-                <img className='fixed top-1/4 left-20 blur-sm opacity-0' ref={scopeStar} src={'icons8-4-point-star-64.png'} />
-                <img className='fixed top-1/3 left-2/3 blur-sm opacity-0' ref={scopeStar2} src={'icons8-4-point-star-64.png'} />
+                <img className='fixed h-[150px] top-1/4 right-40 blur-sm opacity-0' ref={scopeMoon} src={'icons8-moon-90.png'} />
+                <img className='fixed h-[100px] top-1/4 left-20 blur-sm opacity-0' ref={scopeStar} src={'icons8-4-point-star-64.png'} />
+                <img className='fixed h-[100px] top-1/3 left-2/3 blur-sm opacity-0' ref={scopeStar2} src={'icons8-4-point-star-64.png'} />
             </div>
-            <div className={`${styles.paddingX} py-[72px] w-full flex flex-col vh-screen`}>
+            <div className={`px-[150px] py-[72px] w-full flex flex-col vh-screen`}>
                 <div className='h-[calc((100vh-72px)/1.75)] flex items-end'>
                     <div className='overflow-hidden w-full'>
                         <motion.div animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }} initial={{ y: 20, opacity: 0, filter: "blur(5px)" }} transition={{ ease: "easeOut", duration: 0.5, delay: 0.2 }}
